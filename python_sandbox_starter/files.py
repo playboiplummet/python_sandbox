b30c261d7368c5a55773103af3f40c07f5b5b238
playboiplummet <53834030+playboiplummet@users.noreply.github.com>
2020-12-15 12:47:44 -0500

    i finished
    omggggg

--- python_sandbox_starter/files.py
+++ python_sandbox_starter/files.py
@@ -4,4 +4,21 @@
 myFile = open('myFile.txt', 'w')
 
 # Get some info
-print('Name: ' myFile.name)
+print('Name: ', myFile.name)
+print('Is Closed : ', myFile.closed)
+print('Opening Mode: ', myFile.mode)
+
+# Write to file
+myFile.write('I love Python')
+myFile.write(' and Javascript. ')
+myFile.close()
+
+# Append to file
+myFile = open('myFile.txt', 'a')
+myFile.write('I also like PHP.')
+myFile.close()
+
+# Read from file
+myFile = open('myFile.txt', 'r+')
+text = myFile.read(100)
+print(text)
